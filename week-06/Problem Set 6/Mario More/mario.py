14676616fcd1e46362556f6d2430f5898201a741
Davi S <67160664+Davi-S@users.noreply.github.com>
2022-07-20 18:16:08 -0300

    Add files via upload

---
+++ week-06/Problem Set 6/Mario More/mario.py
@@ -0,0 +1,26 @@
+def main():
+    height = get_valid_int_input("Pyramid height: ")
+
+    for i in range(1, height + 1):
+        for j in range(height, i, -1):
+            print(" ", end="")
+        for l in range(i):  # print pyramid again but without spaces to align
+            print("#", end="")
+        print("  ", end="")  # space to separete pyramids
+        for k in range(i):
+            print("#", end="")
+        print()
+
+
+def get_valid_int_input(prompt):
+    while (True):  # loop to get valid input
+        try:
+            user_input = int(input(prompt))
+        except Exception as e:  # exeption to get a valid int
+            continue
+        if 1 <= user_input <= 8:
+            return user_input
+
+
+if __name__ == "__main__":
+    main()
