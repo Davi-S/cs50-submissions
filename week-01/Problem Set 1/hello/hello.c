8d3eb92fd2093eeed9d35aa688c0d088db3a5ca6
Davi S <67160664+Davi-S@users.noreply.github.com>
2022-07-19 22:59:27 -0300

    Delete week-01/Problem Set 1/hello directory

--- week-01/Problem Set 1/hello/hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void)
-{
-    string user_name = get_string("What's your name?\n-> "); // get user's name input
-    printf("hello, %s\n", user_name); // greet usercheck50 cs50/problems/2022/x/hello
-}
